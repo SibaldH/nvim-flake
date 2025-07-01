89eab427a25d83aeb3c4ba0c9f03ff381fe65986
SibaldH <sibald.hulselmans@protonmail.com>
2025-07-01 15:27:37 +0200

    move plugins folder

---
+++ modules/plugins/default.nix
@@ -0,0 +1,7 @@
+{
+  imports = [
+    ./autocomplete.nix
+    ./telescope.nix
+    ./statusline.nix
+  ];
+}
---
+++ modules/plugins/statusline.nix
@@ -0,0 +1,5 @@
+{ pkgs, config, ...}: {
+  vim.statusline.lualine = {
+    enable = true;
+  };
+}
