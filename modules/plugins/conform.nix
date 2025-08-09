41b41197ca32074425d5aaa5edf9bed16e13104c
Sibald Hulselmans <sibald.hulselmans@protonmail.com>
2025-08-09 16:35:28 +0200

    fix formatting error

--- modules/plugins/conform.nix
+++ modules/plugins/conform.nix
@@ -2,7 +2,7 @@
   vim.formatter.conform-nvim = {
     enable = true;
     setupOpts = {
-      default_format_opts = "fallback";
+      default_format_opts = {lsp_format = "fallback";};
       formatter_by_ft = {
         lua = [ "stylua" ];
         rust = [ "rustfmt" ];
