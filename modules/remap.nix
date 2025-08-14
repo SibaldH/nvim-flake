1f83ca119b4b3c68d24d8b27be6db4e5d6e5db62
Sibald Hulselmans <sibald.hulselmans@protonmail.com>
2025-08-14 16:05:06 +0200

    change keymaps
    change keymaps to use integrated way of nvf instaid of luaConfigRC

--- modules/remap.nix
+++ modules/remap.nix
@@ -1,114 +1,134 @@
-{ config, lib, ... }: 
-let 
-        inherit (lib.nvim.binds) mkKeymap;
-in {
-  config.vim.luaConfigRC.remap = ''
-    vim.keymap.set("i", "<C-^>", vim.cmd.stopinsert, { noremap = true, silent = true, desc = "leaves insert mode" }) -- leaves insert mode
-    vim.keymap.set("n", "<leader>w", ":w<Cr>", { noremap = true, silent = false, desc = "write file" }) -- write file
-    vim.keymap.set("n", "<leader>q", ":q<Cr>", { noremap = true, silent = true, desc = "quit nvim" }) -- quit nvim
-    vim.keymap.set("n", "<leader>z", ":wq<Cr>", { noremap = true, silent = false, desc = "write file and quit nvim" }) -- write file and quit nvim
+{
+  config.vim.maps = {
+    normal = {
+      "<leader>w" = {
+        action = "<cmd>w<cr>";
+        desc = "write file";
+      };
+      "<leader>q" = {
+        action = "<cmd>q<cr>";
+        desc = "quit nvim";
+      };
 
-    vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv")
-    vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv")
+      "J".action = "mzJ`z";
+      "<C-d>".action = "<C-d>zz";
+      "<C-u>".action = "<C-u>zz";
+      "n".action = "nzzzv";
+      "N".action = "Nzzzv";
+      "{".action = "{zz";
+      "}".action = "}zz";
+      "Q".action = "<nop>";
+      "<leader>no" = {
+        action = "vim.cmd.noh";
+        desc = "No highlight";
+        lua = true;
+      };
+      "<leader>Y" = {
+        action = "\"+Y";
+        desc = "Yank line into + register";
+      };
+      "<leader>y" = {
+        action = "\"+y";
+        desc = "Yank into + register";
+      };
+      "<leader>d" = {
+        action = "\"_d";
+        desc = "delete to void register";
+      };
+      "[d" = {
+        action = "<cmd>lua vim.diagnostic.goto_next()<cr>zz";
+        desc = "Goto next diagnostic";
+      };
+      "]d" = {
+        action = "<cmd>lua vim.diagnostic.goto_prev()<cr>zz";
+        desc = "Goto previos diagnostic";
+      };
+      "[e" = {
+        action = "<cmd>lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })<cr>zz";
+        desc = "Goto next diagnostic";
+      };
+      "]e" = {
+        action = "<cmd>lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })<cr>zz";
+        desc = "Goto previos diagnostic";
+      };
+      "[w" = {
+        action = "<cmd>lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })<cr>zz";
+        desc = "Goto next diagnostic";
+      };
+      "]w" = {
+        action = "<cmd>lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })<cr>zz";
+        desc = "Goto previos diagnostic";
+      };
+      "<localleader>d" = {
+        action = "<cmd>lua vim.diagnostic.open_float({ border = \"rounded\", })<cr>";
+        desc = "Open the diagnostic under the cursor in a float window";
+      };
+      "<leader>ld" = {
+        action = "vim.diagnostic.setqflist";
+        desc = "Quickfix [L]ist [D]iagnostics";
+        lua = true;
+      };
+      "<leader>cn" = {
+        action = "<cmd>cnext<cr>zz";
+        desc = "Navigate to next qflist item";
+      };
+      "<leader>cp" = {
+        action = "<cmd>cprevious<cr>zz";
+        desc = "Navigate to prev qflist item";
+      };
+      "<leader>co" = {
+        action = "<cmd>copen<cr>zz";
+        desc = "Open qflist";
+      };
+      "<leader>cc" = {
+        action = "<cmd>cclose<cr>zz";
+        desc = "Close qflist";
+      };
+      "U" = {
+        action = "<C-r>";
+        desc = "Redo";
+      };
+      "L" = {
+        action = "$";
+        desc = "Jump to end of line";
+      };
+      "H" = {
+        action = "^";
+        desc = "Jump to start of line";
+      };
+      "S" = {
+        action = "<cmd>%s/\\<C-r><C-w>/\\<C-r><C-w>/gI<Left><Left><Left>";
+        desc = "Quick find/replace for the word under the cursor";
+      };
+    };
 
-    vim.keymap.set("n", "J", "mzJ`z")
-    vim.keymap.set("n", "<C-d>", "<C-d>zz")
-    vim.keymap.set("n", "<C-u>", "<C-u>zz")
-    vim.keymap.set({ "n", "v" }, "{", "{zz")
-    vim.keymap.set({ "n", "v" }, "}", "}zz")
-    vim.keymap.set("n", "n", "nzzzv")
-    vim.keymap.set("n", "N", "Nzzzv")
+    insert = {
+      "<C-^>" = {
+        action = "vim.cmd.stopinsert";
+        desc = "leaves insert mode";
+        lua = true;
+      };
+    };
 
-    -- preserve register 0 when pasing over something
-    vim.keymap.set(
-      "x",
-      "<leader>p",
-      '"_dP',
-      { noremap = true, silent = true, desc = "Paste without overwriting register 0" }
-    )
-
-    -- yank into + register
-    vim.keymap.set({ "v", "n" }, "<leader>y", '"+y', { noremap = true, silent = true, desc = "Yank into + register" })
-    vim.keymap.set("n", "<leader>Y", '"+Y', { noremap = true, silent = true, desc = "Yank line into + register" })
-
-    -- delete to void register
-    vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { noremap = true, silent = true, desc = "Delete to void register" })
-
-    vim.keymap.set("n", "Q", "<nop>")
-
-    vim.keymap.set("n", "<leader>no", vim.cmd.noh, { noremap = true, silent = true, desc = "No highlight" })
-
-    -- Goto next diagnostic of any severity
-    vim.keymap.set("n", "[d", function()
-      vim.diagnostic.goto_next({})
-      vim.api.nvim_feedkeys("zz", "n", false)
-    end, { desc = "Goto next diagnostic" })
-
-    -- Goto previous diagnostic of any severity
-    vim.keymap.set("n", "]d", function()
-      vim.diagnostic.goto_prev({})
-      vim.api.nvim_feedkeys("zz", "n", false)
-    end, { desc = "Goto previous diagnostic" })
-
-    -- Goto next error diagnostic
-    vim.keymap.set("n", "[e", function()
-      vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
-      vim.api.nvim_feedkeys("zz", "n", false)
-    end, { desc = "Goto next error diagnostic" })
-
-    -- Goto previous error diagnostic
-    vim.keymap.set("n", "]e", function()
-      vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
-      vim.api.nvim_feedkeys("zz", "n", false)
-    end, { desc = "Goto previous error diagnostic" })
-
-    -- Goto next warning diagnostic
-    vim.keymap.set("n", "[w", function()
-      vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
-      vim.api.nvim_feedkeys("zz", "n", false)
-    end, { desc = "Goto next warning diagnostic" })
-
-    -- Goto previous warning diagnostic
-    vim.keymap.set("n", "]w", function()
-      vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
-      vim.api.nvim_feedkeys("zz", "n", false)
-    end, { desc = "Goto previous warning diagnostic" })
-
-    -- Open the diagnostic under the cursor in a float window
-    vim.keymap.set("n", "<localleader>d", function()
-      vim.diagnostic.open_float({
-        border = "rounded",
-      })
-    end, { desc = "Open the diagnostic under the cursor in a float window" })
-
-    vim.keymap.set("n", "<leader>ld", vim.diagnostic.setqflist, { desc = "Quickfix [L]ist [D]iagnostics" }) -- Place all dignostics into a qflist
-    vim.keymap.set("n", "<leader>cn", ":cnext<cr>zz", { desc = "Navigate to next qflist item" }) -- Navigate to next
-    vim.keymap.set("n", "<leader>cp", ":cprevious<cr>zz", { desc = "Navigate to previos qflist item" }) -- Navigate to previos qflist item
-    vim.keymap.set("n", "<leader>co", ":copen<cr>zz", { desc = "Open the qflist" }) -- Open the qflist
-    vim.keymap.set("n", "<leader>cc", ":cclose<cr>zz", { desc = "Close the qflist" }) -- Close the qflist
-
-    vim.keymap.set("n", "U", "<C-r>", { noremap = true, silent = true, desc = "Redo" }) -- Redo
-
-    -- Press 'S' for quick find/replace for the word under the cursor
-    vim.keymap.set("n", "S", function()
-      local cmd = ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>"
-      local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
-      vim.api.nvim_feedkeys(keys, "n", false)
-    end, { desc = "Quick find/replace for the word under the cursor" })
-
-    -- Press 'H', 'L' to jump to start/end of a line (first/last char)
-    vim.keymap.set("n", "L", "$", { silent = true, desc = "Jump to end of line" })
-    vim.keymap.set("n", "H", "^", { silent = true, desc = "Jump to start of line" })
-
-    -- Reselect moved text
-    vim.keymap.set("x", ">>", function()
-      vim.cmd("normal! >>")
-      vim.cmd("normal! gv")
-    end)
-
-    vim.keymap.set("x", "<<", function()
-      vim.cmd("normal! <<")
-      vim.cmd("normal! gv")
-    end)
-  '';
+    visual = {
+      "<M-j>".action = "<cmd>m '>+1<cr>gv=gv";
+      "<M-K>".action = "<cmd>m '<-2<cr>gv=gv";
+      "{".action = "{zz";
+      "}".action = "}zz";
+      "<leader>P" = {
+        action = "\"_dP"; # escape string limits with backslach
+        desc = "Paste without overwriting register 0";
+      };
+      "<leader>y" = {
+        action = "\"+y";
+        desc = "Yank into + register";
+      };
+      "<leader>d" = {
+        action = "\"_d";
+        desc = "delete to void register";
+      };
+      "<<".action = "<<gv";
+      ">>".action = ">>gv";
+    };
+  };
 }
