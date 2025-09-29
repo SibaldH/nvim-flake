ba4120f7408b2fd6c6fa707845faa868995e1aa6
SibaldH <sibald.hulselmans@protonmail.com>
2025-09-29 22:32:14 +0200

    support typst language

--- modules/languages.nix
+++ modules/languages.nix
@@ -1,11 +1,14 @@
 {
         config.vim.lsp.enable = true;
         config.vim.languages = {
+                enableFormat = true;
                 enableTreesitter = true;
                 enableDAP = true;
+                enableExtraDiagnostics = true;
                 
                 nix.enable = true;
                 lua.enable = true;
                 rust.enable = true;
+                typst.enable = true;
         };
 }
