9701c17454bf400f4b13883d67943dc2a0b3e8a4
SibaldH <sibald.hulselmans@protonmail.com>
2025-10-08 14:58:58 +0200

    fix(app): better rust language config

--- modules/languages.nix
+++ modules/languages.nix
@@ -1,14 +1,25 @@
 {
-        config.vim.lsp.enable = true;
-        config.vim.languages = {
-                enableFormat = true;
-                enableTreesitter = true;
-                enableDAP = true;
-                enableExtraDiagnostics = true;
-                
-                nix.enable = true;
-                lua.enable = true;
-                rust.enable = true;
-                typst.enable = true;
-        };
+  config.vim.lsp.enable = true;
+  config.vim.languages = {
+    enableFormat = true;
+    enableTreesitter = true;
+    enableDAP = true;
+    enableExtraDiagnostics = true;
+    
+    nix.enable = true;
+    lua.enable = true;
+    typst.enable = true;
+    rust = {
+      enable = true;
+      lsp.opts = ''
+        ['rust-analyzer'] = {
+            cargo = {allFeature = true},
+            checkOnSave = true,
+            procMacro = {
+              enable = true,
+            },
+          },
+      '';
+    };
+  };
 }
